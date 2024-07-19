7df497fa5a43f57c9364097936b442b07b2c205f
Keerthan <keerthangowdas222@gmail.com>
2024-07-20 00:48:41 +0530

    added final project

---
+++ lib/data/themes_data.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData lightModeTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Colors.black,
+  secondaryHeaderColor: const Color(0xff3b22a1),
+  textTheme: TextTheme(
+    bodyMedium: GoogleFonts.poppins(color: Colors.black),
+  ),
+  cardColor: Colors.white//, colorScheme: ColorScheme(background: const Color(0xfff8f8f8)),
+);
+ThemeData darkModeTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.white,
+  secondaryHeaderColor: Colors.white,
+  textTheme: TextTheme(
+    bodyMedium: GoogleFonts.poppins(color: Colors.white),
+  ),
+  cardColor: const Color(0xff070606)//, colorScheme: ColorScheme(background: const Color(0xff06090d), brightness: Brightness.light, primary: null),
+);
